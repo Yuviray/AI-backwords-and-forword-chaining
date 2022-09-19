fbe958fd4f03d2db39ca050fe336a218edd5bf52
jjd149 <jjd149@txstate.edu>
2022-09-19 15:13:15 -0500

    Add files via upload
    Final touches

---
+++ project1/main.cpp
@@ -0,0 +1,28 @@
+
+//
+//
+//
+#include "bc.h"
+#include "fc.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string problem;
+    string solution;
+
+    cout << "Starting backwards chaining code....\n" << endl;
+
+    bc backwards;
+    problem.assign(backwards.start());
+
+    cout << "Done with backwards chaining, your problem is : \n" << problem << endl;
+    cout << "Starting forwards chaining code....\n" << endl ;
+
+    fc forwards(problem);
+    solution.assign(forwards.start());
+
+    cout << "To fix you need to " << solution << " " << problem << endl;
+
+}
